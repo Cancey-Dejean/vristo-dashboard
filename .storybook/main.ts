48d2c03df511020cdb90cfb18271daddd08f1f68
Cancey Dejean <canceyd@gmail.com>
2023-12-24 19:33:17 -0600

    Add @whitespace/storybook-addon-html to package.json and .storybook/main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
     stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)', '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions', '@storybook/addon-a11y'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions', '@storybook/addon-a11y', '@whitespace/storybook-addon-html'],
     framework: {
         name: '@storybook/nextjs',
         options: {},
